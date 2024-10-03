8924256b1ebd31cb5bc02d76586cf013d8e1b01a
Luu Hoang Phuc <luuphuc588@gmail.com>
2024-10-03 22:54:44 +0700

    Log in Register

---
+++ Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlowersShop.Models;
+using FlowersShop.Repository;//Các context, class được lưu ở Repository
+
+
+namespace FlowersShop.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        UserProc obj = new UserProc();
+        QL_BanHoaEntities db = new QL_BanHoaEntities();
+        public ActionResult DangNhap()
+        {
+            if (Session["Signed"] != null)//Signed = Đã đăng nhập
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(Users user)
+        {
+                if (obj.XacThucUser(user))
+                {
+                    //Sử dụng TempData để chuyển sang Action của Controller khác
+                    Session["Signed"] = user.UserName;//Tạo session để biết người dùng đã log in chưa
+                    TempData["user"] = user;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                ViewBag.error = "Sai tên đăng nhập hoặc mật khẩu";
+                    //ModelState.AddModelError("UserName", "Sai tên đăng nhập hoặc mật khẩu");
+                    return View();
+                }
+        }
+        public ActionResult ThanhCong()
+        {
+            ViewBag.username = TempData["user"];
+            return View();
+        }
+        public ActionResult DangKy()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(Users user)
+        {
+            user.Role_id = 2;
+            if (obj.CheckUserName(user.UserName))
+            {
+                ModelState.AddModelError("UserName", "Tên đăng nhập đã tồn tại");//Thêm "Error" vào Model để
+                //kiểm xuất ra màn hình người dùng
+            }
+            if (ModelState.IsValid)//Nếu dòng if ở trên đúng là ModelState.AddModelError được thực thi
+                // thì dòng if này sẽ sai
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                TempData["user"] = user;
+                return RedirectToAction("ThanhCong", "User");
+            }
+            else//Dòng if trên sai thì dòng dưới này sẽ thực hiện
+            {
+                return View();
+                //return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ Models/UserProc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FlowersShop.Models;
+using System.Data.Entity;
+using Microsoft.Ajax.Utilities;
+using FlowersShop.Repository;//Các context, class được lưu ở Repository
+
+namespace FlowersShop.Models
+{
+    public class UserProc
+    {
+        //User Proc, Class này dùng để xử lý kiểm tra đăng nhập, đăng ký, xem có trùng dưới database hay không
+        //TMI: User Proccesor, quá gà để xài từ nào khác :(
+
+        QL_BanHoaEntities db = new QL_BanHoaEntities();
+        public bool CheckUserName(string UserNameStr)
+        {
+
+            if (db.Users.Any(u => u.UserName.Equals(UserNameStr))) return true;
+            return false;
+        }
+        public bool XacThucUser(Users users)
+        {
+            if (CheckUserName(users.UserName) && db.Users.Any(u => u.Password == users.Password))
+            {
+                //Dùng Linq Any để check xem có đúng UserName với Password hay không
+                return true;
+            }
+            return false;
+        }
+        public List<Users> GetUsers() {
+            return db.Users.ToList();
+        }
+    }
+}
